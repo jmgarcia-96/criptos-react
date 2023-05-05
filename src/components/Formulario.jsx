2b3a1b7ba5428cca739fc2cc7bccf2ab24a4f574
jesgarlan <96166634+jmgarcia-96@users.noreply.github.com>
2023-05-05 11:00:36 +0200

    Proyecto terminado

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,75 @@
+import { useEffect, useState } from "react";
+import styled from "@emotion/styled";
+import useSelectMonedas from "../hooks/useSelectMonedas";
+import { monedas } from "../data/monedas";
+import Error from "./Error";
+
+const InputSubmit = styled.input`
+  background-color: #9497ff;
+  border: none;
+  width: 100%;
+  padding: 10px;
+  color: #fff;
+  font-weight: 700;
+  text-transform: uppercase;
+  font-size: 20px;
+  border-radius: 5px;
+  transition: background-color 0.3s ease;
+  margin-top: 30px;
+
+  &:hover {
+    background-color: #7a7dfe;
+    cursor: pointer;
+  }
+`;
+const Formulario = ({ setMonedas }) => {
+  const [error, setError] = useState(false);
+  const [criptos, setCriptos] = useState([]);
+  const [moneda, SelectMonedas] = useSelectMonedas("Elige tu Moneda", monedas);
+  const [criptomoneda, SelectCriptomonedas] = useSelectMonedas(
+    "Elige tu Criptomoneda",
+    criptos
+  );
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url =
+        "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD";
+      const respuesta = await fetch(url);
+      const resultado = await respuesta.json();
+
+      const arrayCriptos = resultado.Data.map((cripto) => {
+        return { id: cripto.CoinInfo.Name, nombre: cripto.CoinInfo.FullName };
+      });
+      setCriptos(arrayCriptos);
+    };
+    consultarAPI();
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if ([moneda, criptomoneda].includes("")) {
+      setError(true);
+      return;
+    }
+
+    setError(false);
+    setMonedas({ moneda, criptomoneda });
+  };
+
+  const conversion = () => {};
+
+  return (
+    <>
+      {error && <Error>Todos los campos son obligatorios</Error>}
+      <form onSubmit={handleSubmit}>
+        <SelectMonedas />
+        <SelectCriptomonedas />
+        <InputSubmit type="submit" value="Cotizar" />
+      </form>
+    </>
+  );
+};
+
+export default Formulario;
